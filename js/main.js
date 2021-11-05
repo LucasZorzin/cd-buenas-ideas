4049e9da66add5ce4ef4b51a725155b3d6426021
Lucas Zorzin <lucaszorzincml@gmail.com>
2021-11-05 19:15:27 -0300

    -responsive: bug fixes

--- js/main.js
+++ js/main.js
@@ -2,28 +2,32 @@
 let link=$('.navbar__item-link');
 
 $('#sobre-mi').click(() => {
-    $(link)[0].classList.toggle("navbar__item-link--active");
+    $(link)[0].classList.add("navbar__item-link--active");
+    setTimeout(()=>{$('#navbar')[0].classList.toggle("navbar-out");},200);
     $(link)[1].classList.remove("navbar__item-link--active");
     $(link)[2].classList.remove("navbar__item-link--active");
     $(link)[3].classList.remove("navbar__item-link--active");
 });
 
 $('#proyectos').click(() => {
-    $(link)[1].classList.toggle("navbar__item-link--active");
+    $(link)[1].classList.add("navbar__item-link--active");
+    setTimeout(()=>{$('#navbar')[0].classList.toggle("navbar-out");},200);
     $(link)[0].classList.remove("navbar__item-link--active");
     $(link)[2].classList.remove("navbar__item-link--active");
     $(link)[3].classList.remove("navbar__item-link--active");
 });
 
 $('#planes').click(() => {
-    $(link)[2].classList.toggle("navbar__item-link--active");
+    $(link)[2].classList.add("navbar__item-link--active");
+    setTimeout(()=>{$('#navbar')[0].classList.toggle("navbar-out");},200);
     $(link)[0].classList.remove("navbar__item-link--active");
     $(link)[1].classList.remove("navbar__item-link--active");
     $(link)[3].classList.remove("navbar__item-link--active");
 });
 
 $('#contactame').click(() => {
-    $(link)[3].classList.toggle("navbar__item-link--active");
+    $(link)[3].classList.add("navbar__item-link--active");
+    setTimeout(()=>{$('#navbar')[0].classList.toggle("navbar-out");},200);
     $(link)[2].classList.remove("navbar__item-link--active");
     $(link)[1].classList.remove("navbar__item-link--active");
     $(link)[0].classList.remove("navbar__item-link--active");
@@ -31,7 +35,7 @@ $('#contactame').click(() => {
 
 // btn open & close
 $('#open-btn').click(() => {
-    $('#navbar')[0].classList.toggle("navbar-in");
+    $('#navbar')[0].classList.add("navbar-in");
     $('#navbar')[0].classList.remove("navbar-out");
 });
 
