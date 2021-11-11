e1bcee6377e08584436e8341068a7a74ee515883
Lucas Zorzin <lucaszorzincml@gmail.com>
2021-11-11 19:42:12 -0300

    -carousel added. -responsive bug fixes. (mobile)

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 //Navbar
 let link=$('.navbar__item-link');
 
-$('#sobre-mi').click(() => {
+$('#sobre-mi-nav').click(() => {
     $(link)[0].classList.add("navbar__item-link--active");
     setTimeout(()=>{$('#navbar')[0].classList.toggle("navbar-out");},200);
     $(link)[1].classList.remove("navbar__item-link--active");
@@ -9,7 +9,7 @@ $('#sobre-mi').click(() => {
     $(link)[3].classList.remove("navbar__item-link--active");
 });
 
-$('#proyectos').click(() => {
+$('#proyectos-nav').click(() => {
     $(link)[1].classList.add("navbar__item-link--active");
     setTimeout(()=>{$('#navbar')[0].classList.toggle("navbar-out");},200);
     $(link)[0].classList.remove("navbar__item-link--active");
@@ -17,7 +17,7 @@ $('#proyectos').click(() => {
     $(link)[3].classList.remove("navbar__item-link--active");
 });
 
-$('#planes').click(() => {
+$('#planes-nav').click(() => {
     $(link)[2].classList.add("navbar__item-link--active");
     setTimeout(()=>{$('#navbar')[0].classList.toggle("navbar-out");},200);
     $(link)[0].classList.remove("navbar__item-link--active");
@@ -25,7 +25,7 @@ $('#planes').click(() => {
     $(link)[3].classList.remove("navbar__item-link--active");
 });
 
-$('#contactame').click(() => {
+$('#contactame-nav').click(() => {
     $(link)[3].classList.add("navbar__item-link--active");
     setTimeout(()=>{$('#navbar')[0].classList.toggle("navbar-out");},200);
     $(link)[2].classList.remove("navbar__item-link--active");
@@ -42,4 +42,44 @@ $('#open-btn').click(() => {
 $('#close-btn').click(() => {
     $('#navbar')[0].classList.remove("navbar-in");
     $('#navbar')[0].classList.toggle("navbar-out");
+});
+
+//// CAROUSEL
+$('.carousel-proyects').slick({
+	dots: true,
+	infinite: false,
+	speed: 900,
+	slidesToShow: 4,
+	slidesToScroll: 1,
+	autoplay: true,
+	autoplaySpeed:4000,
+	arrows: true,
+	responsive: [
+		{
+			breakpoint: 1024,
+			settings: {
+				slidesToShow: 3,
+				slidesToScroll: 3,
+				infinite: true,
+				dots: true
+			}
+		},
+		{
+			breakpoint: 600,
+			settings: {
+				slidesToShow: 2,
+				slidesToScroll: 2
+			}
+		},
+		{
+			breakpoint: 480,
+			settings: {
+				slidesToShow: 1,
+				slidesToScroll: 1
+			}
+		}
+		// You can unslick at a given breakpoint now by adding:
+		// settings: "unslick"
+		// instead of a settings object
+	]
 });
