6e02885f86eba01f3ecc53c68ad2e76bec74a927
Lucas Zorzin <lucaszorzincml@gmail.com>
2021-11-26 19:05:03 -0300

    responsive design bugs fixed

--- js/main.js
+++ js/main.js
@@ -5,59 +5,63 @@ window.onload = () => {
 }
 
 //Navbar
-let link=$('.nav-links');
 
-$('#sobre-mi-nav').click(() => {
-    $(link)[0].classList.toggle("nav-link--active");
-	setTimeout(()=>{$('#navbarResponsive')[0].classList.remove("show");},350);
-	setTimeout(()=>{$(link)[0].classList.remove("nav-link--active");},400);
-    $(link)[1].classList.remove("nav-link--active");
-    $(link)[2].classList.remove("nav-link--active");
-});
+if (window.matchMedia("(max-width: 992px)").matches) {
 
-$('#planes-nav').click(() => {
-    $(link)[1].classList.toggle("nav-link--active");
-	setTimeout(()=>{$('#navbarResponsive')[0].classList.remove("show");},350);
-	setTimeout(()=>{$(link)[1].classList.remove("nav-link--active");},400);
-    $(link)[0].classList.remove("nav-link--active");
-    $(link)[2].classList.remove("nav-link--active");
-});
+    let link = $('.nav-links');
 
-$('#clientes-nav').click(() => {
-    $(link)[2].classList.toggle("nav-link--active");
-	setTimeout(()=>{$('#navbarResponsive')[0].classList.remove("show");},350);
-	setTimeout(()=>{$(link)[2].classList.remove("nav-link--active");},400);
-    $(link)[0].classList.remove("nav-link--active");
-    $(link)[1].classList.remove("nav-link--active");
-});
+    $('#sobre-mi-nav').click(() => {
+        $(link)[0].classList.toggle("nav-link--active");
+        setTimeout(() => { $('#navbarResponsive')[0].classList.remove("show"); }, 350);
+        setTimeout(() => { $(link)[0].classList.remove("nav-link--active"); }, 400);
+        $(link)[1].classList.remove("nav-link--active");
+        $(link)[2].classList.remove("nav-link--active");
+    });
+
+    $('#planes-nav').click(() => {
+        $(link)[1].classList.toggle("nav-link--active");
+        setTimeout(() => { $('#navbarResponsive')[0].classList.remove("show"); }, 350);
+        setTimeout(() => { $(link)[1].classList.remove("nav-link--active"); }, 400);
+        $(link)[0].classList.remove("nav-link--active");
+        $(link)[2].classList.remove("nav-link--active");
+    });
+
+    $('#clientes-nav').click(() => {
+        $(link)[2].classList.toggle("nav-link--active");
+        setTimeout(() => { $('#navbarResponsive')[0].classList.remove("show"); }, 350);
+        setTimeout(() => { $(link)[2].classList.remove("nav-link--active"); }, 400);
+        $(link)[0].classList.remove("nav-link--active");
+        $(link)[1].classList.remove("nav-link--active");
+    });
+}
 
 // Clientes
-$('#figure1').hover( 
-    () => {$('figure')[0].classList.toggle("figure-hover");},
-    () => {$('figure')[0].classList.toggle("figure-hover");}
+$('#figure1').hover(
+    () => { $('figure')[0].classList.toggle("figure-hover"); },
+    () => { $('figure')[0].classList.toggle("figure-hover"); }
 );
 
-$('#figure2').hover( 
-    () => {$('figure')[1].classList.toggle("figure-hover");},
-    () => {$('figure')[1].classList.toggle("figure-hover");}
+$('#figure2').hover(
+    () => { $('figure')[1].classList.toggle("figure-hover"); },
+    () => { $('figure')[1].classList.toggle("figure-hover"); }
 );
 
-$('#figure3').hover( 
-    () => {$('figure')[2].classList.toggle("figure-hover");},
-    () => {$('figure')[2].classList.toggle("figure-hover");}
+$('#figure3').hover(
+    () => { $('figure')[2].classList.toggle("figure-hover"); },
+    () => { $('figure')[2].classList.toggle("figure-hover"); }
 );
 
-$('#figure4').hover( 
-    () => {$('figure')[3].classList.toggle("figure-hover");},
-    () => {$('figure')[3].classList.toggle("figure-hover");}
+$('#figure4').hover(
+    () => { $('figure')[3].classList.toggle("figure-hover"); },
+    () => { $('figure')[3].classList.toggle("figure-hover"); }
 );
 
-$('#figure5').hover( 
-    () => {$('figure')[4].classList.toggle("figure-hover");},
-    () => {$('figure')[4].classList.toggle("figure-hover");}
+$('#figure5').hover(
+    () => { $('figure')[4].classList.toggle("figure-hover"); },
+    () => { $('figure')[4].classList.toggle("figure-hover"); }
 );
 
-$('#figure6').hover( 
-    () => {$('figure')[5].classList.toggle("figure-hover");},
-    () => {$('figure')[5].classList.toggle("figure-hover");}
+$('#figure6').hover(
+    () => { $('figure')[5].classList.toggle("figure-hover"); },
+    () => { $('figure')[5].classList.toggle("figure-hover"); }
 );
