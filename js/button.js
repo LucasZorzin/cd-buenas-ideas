b04f695dbd72ca342fd0ce0c2af5dd94691ed4c4
Lucas Zorzin <lucaszorzincml@gmail.com>
2021-11-25 21:32:46 -0300

    cart bug fixed

--- js/button.js
+++ js/button.js
@@ -2,11 +2,11 @@ $(window).on('scroll', function () {
     if ((window.innerHeight + Math.ceil(window.pageYOffset)) >= document.body.offsetHeight) {
         $('.float').stop(true).animate({ /* Sube 80 px desde el bottom */
             bottom: 80
-        }, 250);
+        }, 150);
     }
     else {
         $('.float').stop(true).animate({ /* En el resto de la página está a 20px */
             bottom: 20
-        }, 200);
+        }, 150);
     }
 });
